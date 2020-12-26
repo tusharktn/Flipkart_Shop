4db0df96f639b8bf4688bd51c2a27b9779d0b809
pratimk999 <pratimkundu15@gmail.com>
2020-12-26 18:01:36 +0530

    server setup && mongo setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,32 @@
 require('dotenv').config();
 const express = require('express');
-// const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+//NOTE Mongodb setup
+
+mongoose.connect(process.env.MONGODB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.on('connected',() => {
+    console.log("DATABASE CONNECTED");
+});
+
+mongoose.connection.on('error',() => {
+    console.log('E R R O R');
+});
+
+app.use(express.json());
+
+
 
 app.use('/',(req,res,next) => {
     res.status(200).json({
         message:'Hello'
-    })
+    });
 });
 app.listen(process.env.PORT || 8000 , () => {
     console.log(`Server is running at ${process.env.port}`);
 });
+
 
