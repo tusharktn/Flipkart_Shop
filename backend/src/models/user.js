07a79c76564311fa22b2147ab2d6ff055a6ce8c5
pratimk999 <pratimkundu15@gmail.com>
2020-12-27 00:46:28 +0530

    user model & passport js implementation

---
+++ backend/src/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    firstName:{type:String , required:true ,trim:true},
+    lastName:{type:String , required:true ,trim:true},
+    email:{type:String , required:true ,unique:true, trim:true},
+    contactNumber:{type:String , required:true ,unique:true, trim:true},
+    role:{type:String , enum:['admin','user'] , default:'admin'},
+    profilePic:{type:String}
+},{timestamps:true});
+
+userSchema.plugin(passportLocalMongoose);
+
+
+module.exports = mongoose.model('User',userSchema);
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+const User = require('./models/user');
+//NOTE USER routes 
+const userRoutes = require('./routes/user');
+
 
 //NOTE Mongodb setup
 
-mongoose.connect(process.env.MONGODB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 mongoose.connection.on('connected',() => {
     console.log("DATABASE CONNECTED");
@@ -17,14 +24,36 @@ mongoose.connection.on('error',() => {
 });
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
+// NOTE Passport js setup
 
-app.use('/',(req,res,next) => {
-    res.status(200).json({
-        message:'Hello'
-    });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// app.get('/fakeUser',async (req,res) => {
+//     try {
+//         const user = new User({
+//             firstName:'Tushar',
+//             lastName:'Khaitan',
+//             email:'pk@gmail.com',
+//             username:'demo',
+//             contactNumber:'56768'
+//         });
+
+//         const newUser = await User.register(user,'8568');
+//         res.send(newUser);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
+
 app.listen(process.env.PORT || 8000 , () => {
     console.log(`Server is running at ${process.env.port}`);
 });
