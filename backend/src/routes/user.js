003c4474e876ed4b496b22b7d7fe7f9b8545a2c1
pratimk999 <pratimkundu15@gmail.com>
2020-12-27 21:50:36 +0530

    auth routes created

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,13 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const passport = require("passport");
 
-
-router.post('/signin', (req,res) => {
-
+//!NOTE SIGNIN ROUTE
+router.post("/signin", passport.authenticate("local"), (req, res) => {
+  User.find({ email: req.body.email }, (err, user) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(user);
+  });
 });
 
-router.post('/signup', (req,res) => {
+//!NOTE SIGNUP ROUTE
+router.post("/signup", async (req, res) => {
+  User.find({ email: req.body.email }, async (err, user) => {
+    if (err) console.log(err);
+    if (user) {
+      return res.json({ message: "user exists" });
+    } else {
+      try {
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const email = req.body.email;
+        const username = req.body.username;
+        const password = req.body.password;
+        const contactNumber = req.body.contactNumber;
 
+        const _user = new User({
+          firstName,
+          lastName,
+          email,
+          username,
+          contactNumber,
+        });
+        const newUser = await User.register(_user, password.toString());
+        console.log("user created");
+        res.send(newUser);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+});
+
+//!NOTE LOGOUT ROUTE
+
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("back");
 });
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,24 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const passportLocalMongoose = require('passport-local-mongoose');
-const User = require('./models/user');
-//NOTE USER routes 
-const userRoutes = require('./routes/user');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("./models/user");
+//!NOTE USER routes
+const userRoutes = require("./routes/user");
 
+app.use(
+  require("express-session")({
+    secret: "Anything",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-//NOTE Mongodb setup
-
-mongoose.connect(process.env.MONGODB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-
-mongoose.connection.on('connected',() => {
-    console.log("DATABASE CONNECTED");
-});
-
-mongoose.connection.on('error',() => {
-    console.log('E R R O R');
-});
-
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-
-// NOTE Passport js setup
-
+// !NOTE Passport js setup
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -36,26 +26,27 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//!NOTE Mongodb setup
 
-// app.get('/fakeUser',async (req,res) => {
-//     try {
-//         const user = new User({
-//             firstName:'Tushar',
-//             lastName:'Khaitan',
-//             email:'pk@gmail.com',
-//             username:'demo',
-//             contactNumber:'56768'
-//         });
-
-//         const newUser = await User.register(user,'8568');
-//         res.send(newUser);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
-
-app.listen(process.env.PORT || 8000 , () => {
-    console.log(`Server is running at ${process.env.port}`);
+mongoose.connect(process.env.MONGODB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
+mongoose.connection.on("connected", () => {
+  console.log("DATABASE CONNECTED");
+});
 
+mongoose.connection.on("error", () => {
+  console.log("E R R O R");
+});
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(userRoutes);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running at ${process.env.PORT}`);
+});
